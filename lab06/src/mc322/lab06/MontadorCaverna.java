93816c3d9c7096404efb530c7d084dcd24a2f2f0
NomeFiltrado1587 <viniluizfarias@gmail.com>
2021-05-07 01:56:53 -0300

    Salas iniciando exploradas

--- lab06/src/mc322/lab06/MontadorCaverna.java
+++ lab06/src/mc322/lab06/MontadorCaverna.java
@@ -63,6 +63,7 @@ public class MontadorCaverna {
 		
 		for(int linha =0;linha <4;linha++) {
 			for(int coluna=0;coluna<4;coluna ++) {
+				
 				tabuleiroCaverna[linha][coluna] = new Sala();
 				tabuleiroCaverna[linha][coluna].setExplorada(true);
 			}
